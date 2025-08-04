e174569f7ca7f571facffeaf75d9d23400ca0b67
Mark Servilla <mark.servilla@gmail.com>
2025-08-04 02:39:43 +0000

    Build out service.py (incomplete)

--- src/iam_migrate/service.py
+++ src/iam_migrate/service.py
@@ -18,19 +18,66 @@ import logging
 from pathlib import Path
 
 import daiquiri
+from lxml import etree
+from iam_lib.exceptions import IAMResponseError
+from iam_lib.api.profile import ProfileClient
+from iam_lib.api.resource import ResourceClient
+from iam_lib.api.rule import RuleClient
+from iam_lib.models.permission import Permission, PERMISSION_MAP
 
+from config import Config
+import jwt_token
 
-CWD = Path(".").resolve().as_posix()
-LOGFILE = CWD + "/migrate.log"
-daiquiri.setup(
-    level=logging.INFO,
-    outputs=(
-        daiquiri.output.File(LOGFILE),
-        "stdout",
-    ),
-)
 logger = daiquiri.getLogger(__name__)
 
 
-def migrate(service_file: Path):
-    pass
+def migrate(service_file: str):
+    """
+    Migrate a service API authorization state to the IAM service.
+    Reads and parses the XML configuration file.
+    
+    Args:
+        service_file: Path to the service XML file
+    """
+    namespaces = {
+        'pasta': 'pasta://pasta.edirepository.org/service-0.1'
+    }
+
+    client_token = jwt_token.make_token(Config.CLIENT_ID)
+
+    root = etree.parse(service_file)
+    service_methods = root.findall("pasta:service-method", namespaces)
+    for method in service_methods:
+        service_name = method.get("name")
+        print(service_name)
+        allows = method.findall(".//access/allow", namespaces)
+        for allow in allows:
+            principal = allow.find("principal", namespaces).text
+            permission = allow.find("permission", namespaces).text
+            print(f"    {principal} - {permission}")
+
+
+def _resource_client(token: str):
+    return ResourceClient(
+        scheme=Config.SCHEME,
+        host=Config.AUTH_HOST,
+        accept=Config.ACCEPT,
+        public_key_path=Config.PUBLIC_KEY_PATH,
+        algorithm=Config.JWT_ALGORITHM,
+        token=token,
+        truststore=Config.TRUSTSTORE,
+        timeout=Config.CONNECT_TIMEOUT,
+    )
+
+
+def _rule_client(token: str):
+    return RuleClient(
+        scheme=Config.SCHEME,
+        host=Config.AUTH_HOST,
+        accept=Config.ACCEPT,
+        public_key_path=Config.PUBLIC_KEY_PATH,
+        algorithm=Config.JWT_ALGORITHM,
+        token=token,
+        truststore=Config.TRUSTSTORE,
+        timeout=Config.CONNECT_TIMEOUT,
+    )
