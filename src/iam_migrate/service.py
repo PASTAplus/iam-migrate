3e855d5e7ffde1c9c578c0c0b45fc900b81b61bc
Mark Servilla <mark.servilla@gmail.com>
2025-08-04 21:48:58 +0000

    Build service-level ACRs

--- src/iam_migrate/service.py
+++ src/iam_migrate/service.py
@@ -26,6 +26,7 @@ from iam_lib.api.rule import RuleClient
 from iam_lib.models.permission import Permission, PERMISSION_MAP
 
 from config import Config
+from edi_id import PASTA_ID_MAP, EDI_ID_MAP
 import jwt_token
 
 logger = daiquiri.getLogger(__name__)
@@ -44,23 +45,39 @@ def migrate(service_file: str):
     }
 
     client_token = jwt_token.make_token(Config.CLIENT_ID)
+    resource_client = _resource_client(client_token)
+    rule_client = _rule_client(client_token)
+
 
     root = etree.parse(service_file)
     service_methods = root.findall("pasta:service-method", namespaces)
     for method in service_methods:
         service_name = method.get("name")
         print(service_name)
+        resource_client.create_resource(
+            resource_key=f"{Config.PACKAGE_HOST}:{Config.PACKAGE_SERVICE}:{service_name}",
+            resource_type="service",
+            resource_label=f"{Config.PACKAGE_SERVICE}:{service_name}",
+            parent_resource_key=None
+        )
         allows = method.findall(".//access/allow", namespaces)
         for allow in allows:
             principal = allow.find("principal", namespaces).text
             permission = allow.find("permission", namespaces).text
-            print(f"    {principal} - {permission}")
+            if principal in ("public", "authenticated", "vetted"):
+                edi_id = EDI_ID_MAP[PASTA_ID_MAP.index(principal)]
+                print(f"    {principal} ({edi_id}) - {permission}")
+                rule_client.create_rule(
+                    resource_key=f"{Config.PACKAGE_HOST}:{Config.PACKAGE_SERVICE}:{service_name}",
+                    principal=edi_id,
+                    permission=Permission(PERMISSION_MAP.index(permission)),
+                )
 
 
 def _resource_client(token: str):
     return ResourceClient(
         scheme=Config.SCHEME,
-        host=Config.AUTH_HOST,
+        host=f"{Config.AUTH_HOST}:{Config.AUTH_PORT}",
         accept=Config.ACCEPT,
         public_key_path=Config.PUBLIC_KEY_PATH,
         algorithm=Config.JWT_ALGORITHM,
@@ -73,7 +90,7 @@ def _resource_client(token: str):
 def _rule_client(token: str):
     return RuleClient(
         scheme=Config.SCHEME,
-        host=Config.AUTH_HOST,
+        host=f"{Config.AUTH_HOST}:{Config.AUTH_PORT}",
         accept=Config.ACCEPT,
         public_key_path=Config.PUBLIC_KEY_PATH,
         algorithm=Config.JWT_ALGORITHM,
